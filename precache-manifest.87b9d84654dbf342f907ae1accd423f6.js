57719a8c198dcaf7cf075c62119f5d923e6ec4c2
karan <karan@HGBAUN4.local>
2019-01-09 15:57:47 +0530

    Updates

---
+++ precache-manifest.87b9d84654dbf342f907ae1accd423f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "56248502e56b384b7e4d",
+    "url": "/static/css/main.50b5e792.chunk.css"
+  },
+  {
+    "revision": "56248502e56b384b7e4d",
+    "url": "/static/js/main.56248502.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ff17c28177b5bfb1fd69998aa4a65c3b",
+    "url": "/index.html"
+  }
+];
